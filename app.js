85f10b19a30a8629096ef4016637306cd7592ee2
Derek Wolpert <dfnj123@gmail.com>
2020-01-28 16:09:07 -0500

    config'ed test routes

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
